ab445b70195fd3cc9c84b16f9587eb55484515a5
Guilherme Artigas <guilherme.artigas92@gmail.com>
2024-02-22 09:00:41 -0300

    refactor: alterando texto da tela de login

--- src/pages/Signin/index.js
+++ src/pages/Signin/index.js
@@ -4,7 +4,7 @@ import { View, Text } from 'react-native';
 export default function SignIn() {
   return (
     <View>
-      <Text>Tela login</Text>
+      <Text>Tela login!</Text>
     </View>
   )
 }
