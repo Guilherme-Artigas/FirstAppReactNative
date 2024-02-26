759ed7347a41e68d3dc434cdb8624d3e9b3622cb
Guilherme Artigas <guilherme.artigas92@gmail.com>
2024-02-26 08:23:44 -0300

    feat: header na tela de login

--- src/pages/Signin/index.js
+++ src/pages/Signin/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import * as Animatable from 'react-native-animatable';
 
 export default function SignIn() {
   return (
-    <View>
-      <Text>Tela login!</Text>
+    <View style={styles.container}>
+      <Animatable.View animation="fadeInLeft" delay={500}>
+        <Text>Bem vindo(a)</Text>
+      </Animatable.View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {}
+});
