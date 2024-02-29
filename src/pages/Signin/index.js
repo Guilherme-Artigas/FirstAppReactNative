736819cd03d8ada34ac65b5ae26656811282a98d
Guilherme Artigas <guilherme.artigas92@gmail.com>
2024-02-29 09:22:42 -0300

    feat: adicioanndo campos de email e senha

--- src/pages/Signin/index.js
+++ src/pages/Signin/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TextInput } from 'react-native';
 import * as Animatable from 'react-native-animatable';
 
 export default function SignIn() {
@@ -7,6 +7,22 @@ export default function SignIn() {
     <View style={styles.container}>
       <Animatable.View animation="fadeInLeft" delay={500} style={styles.containerHeader}>
         <Text style={styles.message}>Bem vindo(a)</Text>
+      </Animatable.View>
+
+      <Animatable.View animation="fadeInUp" style={styles.containerForm}>
+
+        <Text style={styles.title}>Email</Text>
+        <TextInput
+          placeholder="Digite um email..."
+          style={styles.input}
+        />
+
+        <Text style={styles.title}>Senha</Text>
+        <TextInput
+          placeholder="Sua senha"
+          style={styles.input}
+        />
+
       </Animatable.View>
     </View>
   )
